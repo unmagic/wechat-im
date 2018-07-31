01155287ec9c3248312a9a9238777d1fd90c302a
liubiao <liubiao@vlifepaper.com>
2018-07-31 15:38:23 +0800

    两条信息大于5分钟显示后者信息的时间

--- pages/list/im-operator.js
+++ pages/list/im-operator.js
@@ -1,3 +1,5 @@
+import {dealChatTime} from "../../utils/time";
+
 export default class IMOperator {
     static VoiceType = 'voice';
     static TextType = 'text';
@@ -6,12 +8,13 @@ export default class IMOperator {
 
     constructor() {
         this._latestTImestamp = 0;
-        this._myHeadUrl = '../../image/my_head.jpeg'
+        this._myHeadUrl = '../../image/my_head.jpeg';
+        this._otherHeadUrl = '../../image/other_head.jpg';
     }
 
 
     onSimulateReceiveMsg(cbOk) {
-
+        this.onSimulateReceiveMsgCb = cbOk;
     }
 
     onSimulateSendMsg(content, cbOk, cbError) {
@@ -20,6 +23,11 @@ export default class IMOperator {
             const item = this.createNormalChatItem(JSON.parse(content));
             this._latestTImestamp = item.timestamp;
             cbOk && cbOk(item);
+            setTimeout(() => {
+                const item = this.createNormalChatItem({type: 'text', content: '我是自动回复的好友文本消息', isMy: false});
+                this._latestTImestamp = item.timestamp;
+                this.onSimulateReceiveMsgCb && this.onSimulateReceiveMsgCb(item);
+            }, 1000);
         }, 300)
     }
 
@@ -28,20 +36,18 @@ export default class IMOperator {
         return JSON.stringify({content, type, duration});
     }
 
-    createDefaultChaItem() {
-
-    }
-
-    createNormalChatItem({type = IMOperator.TextType, content = '', duration} = {}) {
+    createNormalChatItem({type = IMOperator.TextType, content = '', isMy = true, duration} = {}) {
         if (!content) return;
+        const currentTimestamp = Date.now();
+        const time = dealChatTime(currentTimestamp, this._latestTImestamp);
         return {
-            isMy: true,
-            showTime: true,//是否显示该次发送时间
-            time: '09:15',//发送时间 如 09:15,
-            timestamp: Date.now(),//该条数据的时间戳，一般用于排序
+            isMy: isMy,
+            showTime: time.ifShowTime,//是否显示该次发送时间
+            time: time.timeStr,//发送时间 如 09:15,
+            timestamp: currentTimestamp,//该条数据的时间戳，一般用于排序
             type: type,//内容的类型，目前有这几种类型： text/voice/image/custom | 文本/语音/图片/自定义
             content: content,// 显示的内容，根据不同的类型，在这里填充不同的信息。
-            headUrl: this._myHeadUrl,//显示的头像，你可以填充不同的头像，来满足群聊的需求
+            headUrl: isMy ? this._myHeadUrl : this._otherHeadUrl,//显示的头像，你可以填充不同的头像，来满足群聊的需求
             sendStatus: 'success',//发送状态，目前有这几种状态：sending/success/failed | 发送中/发送成功/发送失败
             voiceDuration: duration,//语音时长 单位秒
             isPlaying: false//语音是否正在播放
@@ -57,8 +63,5 @@ export default class IMOperator {
         }
     }
 
-    getChatItem() {
-
-    }
 }
 
