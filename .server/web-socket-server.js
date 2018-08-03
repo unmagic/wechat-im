94d3b89eb48851452c9001e2a7ee9e30eedf4900
liubiao <liubiao@vlifepaper.com>
2018-08-03 16:29:55 +0800

    完成服务端自动回复功能

--- .server/web-socket-server.js
+++ .server/web-socket-server.js
@@ -1,5 +1,3 @@
-import IMOperator from "../pages/chat/im-operator";
-
 let ws = require("nodejs-websocket");
 
 class WebSocketServer {
@@ -13,21 +11,21 @@ class WebSocketServer {
             conn.on("text", function (str) {
                 console.log("收到的信息为:" + str);
                 let msg = JSON.parse(str);
-                let callbackMsg = '';
+                let callbackMsg = null;
                 switch (msg.type) {
-                    case IMOperator.CustomType:
-                        callbackMsg = IMOperator.createChatItemContent({type: msg.type, content: '这是来自服务器的自定义消息'});
+                    case 'custom':
+                        callbackMsg = {type: msg.type, content: '这是来自服务器的自定义消息'};
                         break;
-                    case IMOperator.ImageType:
-                        callbackMsg = IMOperator.createChatItemContent({type: msg.type, content: '这里可以配置图片url来模拟回复图片消息'});
+                    case 'image':
+                        callbackMsg = {type: msg.type, content: '.gif/效果图2.png'};
                         break;
-                    case IMOperator.VoiceType:
-                        callbackMsg = IMOperator.createChatItemContent({type: msg.type, content: '这里可以配置语音url来模拟回复语音消息'});
+                    case 'voice':
+                        callbackMsg = {type: 'text', content: '这里可以配置图片url来模拟回复图片消息'};
                         break;
-                    case IMOperator.TextType:
-                        callbackMsg = IMOperator.createChatItemContent({type: msg.type, content: '这是来自服务器的自定义消息'});
+                    case 'text':
+                        callbackMsg = {type: msg.type, content: '你好呀，我是WebSocket服务器自动回复的消息'}
                 }
-                conn.sendText(str)
+                conn.sendText(JSON.stringify(callbackMsg));
             })
             conn.on("close", function (code, reason) {
                 console.log("关闭连接")
@@ -36,7 +34,7 @@ class WebSocketServer {
                 console.log("异常关闭", code, reason)
             });
         }).listen(8001);
-        console.log("WebSocket建立完毕")
+        console.log("WebSocket服务端建立完毕")
     }
 }
 
