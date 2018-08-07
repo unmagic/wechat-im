2c1e93b80ca8de2d8b53636619baa1d723cb24af
liubiao <liubiao@vlifepaper.com>
2018-08-07 16:06:10 +0800

    删除无用的字段

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ import IMWebSocket from "./modules/im-sdk/im-web-socket";
 App({
     globalData: {
         userInfo: {},
-        friendsId: [],
-        currentSocketAction: 1,//Socket获取信息状态 1:获取聊天列表信息，2获取会话内容
     },
     getIMWebSocket() {
         return this.imWebSocket;
