2cf284c750a53b9ae3917615e58f1aa1bb3b0f33
liubiao <liubiao@vlifepaper.com>
2018-09-18 16:25:26 +0800

    单例模式获取IMHandler

--- modules/im-sdk/im-factory.js
+++ modules/im-sdk/im-factory.js
@@ -1,7 +1,3 @@
 import WebSocketHandlerImp from "./sdk/web-socket-handler-imp";
 
-export default class IMFactory {
-    static create() {
-        return new WebSocketHandlerImp();
-    }
-}
+export const getIMHandlerFactory = new WebSocketHandlerImp();
