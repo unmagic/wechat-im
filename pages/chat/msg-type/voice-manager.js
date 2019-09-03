07a7881d777c93cd9d5cae17cd582eb317f366ed
liubiao <liubiao@vlifepaper.com>
2019-09-03 18:09:12 +0800

    修复语音有时不能正常播放的问题

--- pages/chat/msg-type/voice-manager.js
+++ pages/chat/msg-type/voice-manager.js
@@ -1,11 +1,11 @@
-import {isVoiceRecordUseLatestVersion} from "../../../modules/chat-input/chat-input";
 import IMOperator from "../im-operator";
 import FileManager from "./base/file-manager";
 
-export default class VoiceManager extends FileManager{
+export default class VoiceManager extends FileManager {
     constructor(page) {
         super(page);
-        this.isLatestVersion = isVoiceRecordUseLatestVersion();
+        this.isLatestVersion = !!wx.getRecorderManager;
+
         //判断是否需要使用高版本语音播放接口
         if (this.isLatestVersion) {
             this.innerAudioContext = wx.createInnerAudioContext();
@@ -21,19 +21,21 @@ export default class VoiceManager extends FileManager{
     /**
      * 停止播放所有语音
      */
-    stopAllVoicePlay() {
+    stopAllVoicePlay(isUpdateView = true) {
         let that = this._page;
         if (this._page.data.isVoicePlaying) {
             this._stopVoice();
-            that.data.chatItems.forEach(item => {
-                if (IMOperator.VoiceType === item.type) {
-                    item.isPlaying = false
-                }
-            });
-            that.setData({
-                chatItems: that.data.chatItems,
-                isVoicePlaying: false
-            })
+            if (isUpdateView) {
+                that.data.chatItems.forEach(item => {
+                    if (IMOperator.VoiceType === item.type) {
+                        item.isPlaying = false
+                    }
+                });
+                that.setData({
+                    chatItems: that.data.chatItems,
+                    isVoicePlaying: false
+                })
+            }
         }
     }
 
@@ -58,20 +60,20 @@ export default class VoiceManager extends FileManager{
             let localPath = dataset.voicePath;//优先读取本地路径，可能不存在此文件
 
             this._myPlayVoice(localPath, dataset, function () {
-                console.log('成功读取了本地语音');
+                // console.log('成功读取了本地语音');
             }, () => {
                 console.log('读取本地语音文件失败，一般情况下是本地没有该文件，需要从服务器下载');
                 wx.downloadFile({
                     url: dataset.voicePath,
                     success: res => {
-                        console.log('下载语音成功', res);
+                        // console.log('下载语音成功', res);
                         this.__playVoice({
                             filePath: res.tempFilePath,
                             success: () => {
                                 this.stopAllVoicePlay();
                             },
                             fail: (res) => {
-                                console.log('播放失败了', res);
+                                // console.log('播放失败了', res);
                             }
                         });
                     }
@@ -107,37 +109,37 @@ export default class VoiceManager extends FileManager{
 
     _myPlayVoice(filePath, dataset, cbOk, cbError) {
         let that = this._page;
-        if (dataset.isMy || that.data.isAndroid) {
-            this.__playVoice({
-                filePath: filePath,
-                success: () => {
-                    this.stopAllVoicePlay();
-                    typeof cbOk === "function" && cbOk();
-                },
-                fail: (res) => {
-                    console.log('播放失败了1', res);
-                    typeof cbError === "function" && cbError(res);
-                }
-            });
-        } else {
-            wx.downloadFile({
-                url: dataset.voicePath,
-                success: res => {
-                    console.log('下载语音成功', res);
-                    this.__playVoice({
-                        filePath: res.tempFilePath,
-                        success: () => {
-                            this.stopAllVoicePlay();
-                            typeof cbOk === "function" && cbOk();
-                        },
-                        fail: (res) => {
-                            console.log('播放失败了', res);
-                            typeof cbError === "function" && cbError(res);
-                        }
-                    });
-                }
-            });
-        }
+        // if (dataset.isMy || that.data.isAndroid) {
+        this.__playVoice({
+            filePath: filePath,
+            success: () => {
+                this.stopAllVoicePlay();
+                typeof cbOk === "function" && cbOk();
+            },
+            fail: (res) => {
+                // console.log('播放失败了1', res);
+                typeof cbError === "function" && cbError(res);
+            }
+        });
+        // } else {
+        //     wx.downloadFile({
+        //         url: dataset.voicePath,
+        //         success: res => {
+        //             // console.log('下载语音成功', res);
+        //             this.__playVoice({
+        //                 filePath: res.tempFilePath,
+        //                 success: () => {
+        //                     this.stopAllVoicePlay();
+        //                     typeof cbOk === "function" && cbOk();
+        //                 },
+        //                 fail: (res) => {
+        //                     // console.log('播放失败了2', res);
+        //                     typeof cbError === "function" && cbError(res);
+        //                 }
+        //             });
+        //         }
+        //     });
+        // }
 
     }
 
